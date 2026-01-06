f46544a0eb0feae64f622a44e0c30f35bd777af4
Mouad El Ouichouani <elouichouanimouad@gmail.com>
2026-01-07 00:11:35 +0100

    feat/add-food-cards

--- app/src/main/java/project/ma/lada/di/AppContainer.kt
+++ app/src/main/java/project/ma/lada/di/AppContainer.kt
@@ -6,11 +6,14 @@ import project.ma.lada.domain.repository.GreetingRepository
 import project.ma.lada.domain.usecase.GetGreetingUseCase
 import project.ma.lada.domain.repository.AuthRepository
 import project.ma.lada.data.repository.AuthRepositoryImpl
+import project.ma.lada.domain.repository.RecipeRepository
+import project.ma.lada.data.repository.RecipeRepositoryImpl
 
 interface AppContainer {
     val greetingRepository: GreetingRepository
     val getGreetingUseCase: GetGreetingUseCase
     val authRepository: AuthRepository
+    val recipeRepository: RecipeRepository
 }
 
 class AppDataContainer : AppContainer {
@@ -26,5 +29,9 @@ class AppDataContainer : AppContainer {
 
     override val authRepository: AuthRepository by lazy {
         AuthRepositoryImpl(com.google.firebase.auth.FirebaseAuth.getInstance())
+    }
+
+    override val recipeRepository: RecipeRepository by lazy {
+        RecipeRepositoryImpl(com.google.firebase.firestore.FirebaseFirestore.getInstance())
     }
 }
